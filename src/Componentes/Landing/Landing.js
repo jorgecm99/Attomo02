2dfbd95623620c8889cd2dbe9a703ccc9009956b
Jorge Corcoles <jcorcoles86@gmail.com>
2022-05-01 00:44:06 +0200

    parallax

--- src/Componentes/Landing/Landing.js
+++ src/Componentes/Landing/Landing.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import videoGranada from '../../Assets/Video/TOURGCF.mp4'
+import './Landing.css';
+import videoGranada from '../../Assets/Video/TOURGCF.mp4';
+import { Parallax, ParallaxLayer } from '@react-spring/parallax';
 
 function Landing() {
   return (
@@ -10,11 +12,28 @@ function Landing() {
             Tu navegador no soporta los vídeos de HTML5
             </video>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div className='parallax-container'>
+        <Parallax pages={2}>
+            <ParallaxLayer speed={8}>
+              <h2>¿qué es?</h2>
+              <p>Una <b>experiencia única</b> que permite al visitante sumergirse en la vibrante emoción del día de partido, 
+                conocer los secretos del club, interactuar con sus jugadores y muchas otras sorpresas.
+              </p>
+              <p>Conoce los <b>secretos</b> del estadio y el club mientras juegas e interactúas a través de experiencias 
+                de realidad aumentada y virtual para todos los públicos. 
+              </p>
+            </ParallaxLayer>
+
+            <ParallaxLayer offset={1}speed={0.5}>
+              <h2>¿qué incluye?</h2>
+              <h3>mural interactivo</h3>
+              <p>Accede a contenido adicional como momentos históricos o  jugadores míticos 
+                del Granada CF a través de la realidad aumentada. 
+              </p>
+            </ParallaxLayer>
+          </Parallax>
+    </div>
+        
     </section>
   )
 }
