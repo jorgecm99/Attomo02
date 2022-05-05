1a742d1831b1c512fbd03c56065d28b7fb7a8de6
Jorge Corcoles <jcorcoles86@gmail.com>
2022-05-05 17:05:58 +0200

    changes

--- src/Componentes/Core/Header.js
+++ src/Componentes/Core/Header.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useState} from 'react';
 import './Header.css';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faX } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import headerLogo from '../../Assets/Images/Mobile/Group 38.png';
 import videoGranada from '../../Assets/Video/TOURGCF.mp4';
+import  logoGranada from '../../Assets/Images/Mobile/logogranada.png';
+import  logoBstadium from '../../Assets/Images/Mobile/logobstadium.png';
+import  wasap from '../../Assets/Images/Desktop/Vector.svg'
+
 
 
 function Header() {
+
+    
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    
+    
+
     const scrollTo = (nodeSelector) => {
         const domNode = document.querySelector(nodeSelector)
 
@@ -17,7 +28,7 @@ function Header() {
     <header>
         <div className='header'>
 
-            <video muted controls autoPlay className='back-video'>
+            <video muted loop autoPlay className='back-video'>
                 <source src={videoGranada} type="video/mp4"></source>
                 Tu navegador no soporta los vídeos de HTML5
             </video>
@@ -25,14 +36,26 @@ function Header() {
             <nav>
             <div className='dropdown'>
                 <div className='dropdown-icon'>
-                    <FontAwesomeIcon icon={faBars} style={{color:"white"}}/>
+
+                {
+                    isMenuOpen ?
+                    <FontAwesomeIcon icon={faX} style={{color:"white"}}/>
+                    : <FontAwesomeIcon icon={faBars} style={{color:"white"}} onClick={()=>setIsMenuOpen(!isMenuOpen)}/>
+                }
+                    
+                    
                 </div>
 
-                <div className='dropdown-content'>
-                    <li>Mural interactivo</li>
-                    <li>Experiencia inmersiva 360°</li>
-                    <li>Foto con jugadores</li>
-                </div>
+                {
+                    isMenuOpen ? 
+                    <div className='dropdown-content'>
+                        <li onClick={() => scrollTo('.desktop-two')}>Mural interactivo</li>
+                        <li onClick={() => scrollTo('.desktop-three')}>Experiencia inmersiva 360°</li>
+                        <li onClick={() => scrollTo('.desktop-four')}>Foto con jugadores</li>
+                    </div> : null
+                } 
+
+                
             </div>
 
             
@@ -41,15 +64,37 @@ function Header() {
 
             <div className='dropdown-content-desktop'>
                     <li onClick={() => scrollTo('.desktop-one')}>¿Qué es?</li>
-                    <li onClick={() => scrollTo('.desktop-two')}>¿Qué incluye?</li>
+                    <div className='dropdown-header'>
+                        <li>¿Qué incluye?</li>    
+                    <div className='dropdown-content-header'>
+                        <li onClick={() => scrollTo('.desktop-two')}>Mural interactivo</li>
+                        <li onClick={() => scrollTo('.desktop-three')}>Experiencia inmersiva 360°</li>
+                        <li onClick={() => scrollTo('.desktop-four')}>Foto con jugadores</li>
+                    </div>
+                </div>  
                     <li onClick={() => scrollTo('.landing-last')}>Info Práctica</li>
             </div>
             
             <div className='btn-container'>
-                <button>Comprar entradas</button>
+                <button onClick={() => window.open("https://bstadium.es/granada-cf/tour-por-el-nuevo-los-carmenes-estadio-de-granada-cf/")}>
+                    Comprar entradas
+                </button>
             </div>
 
             </nav>
+
+            <div className='header-video-content'>
+                <h2 className='header-title'>tour interactivo</h2>
+                <h2 className='header-title-two'>experiencia inmersiva</h2>
+                <button className='header-btn'><a target="_blank" rel="noopener noreferrer" className='footer-btn' href='https://bstadium.es/granada-cf/tour-por-el-nuevo-los-carmenes-estadio-de-granada-cf/' >comprar</a></button>
+                <div className='header-two-images'>
+                    <img className='logo-one' src={logoGranada}alt=''></img>
+                    <img className='logo-one' src={logoBstadium} alt=''></img>
+                </div>
+                <div className='wasap-logo'>
+                    <img  className='wasap-pic' src={wasap} alt=''></img>
+                </div>
+            </div>
 
             
             
